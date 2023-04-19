4a54d0ab42fd774075c0631668b72511bc2fae9a
sophiaqho <54969482+sophiaqho@users.noreply.github.com>
2023-04-18 22:44:47 -0400

    added parsing for player names and max year

--- web_crawler.py
+++ web_crawler.py
@@ -11,7 +11,9 @@ import time
 
 # threshold for whether to add url to queue or not
 threshold = 10
-acceptable_years = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030']
+screening_years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018',
+                    '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030']
+
 
 class WebCrawler:
 
@@ -23,16 +25,17 @@ class WebCrawler:
         self.urls_queue = PriorityQueue()
 
         data = pd.read_csv('player_names.txt', delimiter="\t", header=0).values
-        player_list = np.reshape(data, -1)
+        self.player_list = np.reshape(data, -1)
 
-        player_popularity = {}
+        self.player_popularity = {}
 
         # initialize the count vector to be 0 for each player
-        for player in player_list:
-            player_popularity[player] = 0
+        for player in self.player_list:
+            self.player_popularity[player] = 0
 
         # create a logger file name and use that file to log crawling activities
-        self.logname = "web_crawler_logs/WebCrawler" + "_" + str(datetime.now()) + ".txt"
+        self.logname = "web_crawler_logs/WebCrawler" + \
+            "_" + str(datetime.now()) + ".txt"
         logging.basicConfig(
             filename=self.logname,
             format='%(asctime)s,%(msecs)03d, %(message)s',
@@ -67,12 +70,33 @@ class WebCrawler:
 
     def get_player_info_and_date(self, html):
         soup = BeautifulSoup(html, 'html.parser')
-        html_text = soup.get_text()
-        # TODO read the text file to find the latest year mentioned and the players
-        latest_year = 2020
-        player_count = 3
-        return player_count, latest_year
+        html_text = soup.get_text().lower()
+        # compute the counts of each of the years
+        year_counts = []
+        for year in screening_years:
+            year_counts.append(html_text.count(year))
+        non_zero_arr = np.nonzero(year_counts)[0]
+        # if there is a year mentioned, compute the max year
+        if non_zero_arr:
+            # find the latest year where the count isn't 0
+            max_year = screening_years[max(non_zero_arr)]
+        else:
+            max_year = 2010
+        # TODO add mutexes
 
+        player_count = 0
+        
+        # for each of the top 50 players that we want to search for
+        # read the text file to find the latest year mentioned and the players
+        for player in self.player_list:
+            count_from_cur_site = html_text.count(player)
+            cur_count = self.player_popularity[player]
+            self.player_popularity[player] = count_from_cur_site + cur_count
+            if count_from_cur_site > 0:
+                player_count += 1
+
+        # return the count of the players mentioned and the maximum year mentioned
+        return player_count, int(max_year)
 
     def crawl_url(self, url):
         # if we have not visited this url before, then we can crawl it for information
@@ -83,18 +107,20 @@ class WebCrawler:
             # get_player_info_and_date
             player_count, date = self.get_player_info_and_date(html)
             weight = self.compute_url_weight(player_count, date)
-            if weight < threshold: 
+            if weight < threshold:
                 for link in self.get_hyperlinks(url, html):
                     if link is not None and not link.startswith('#'):
                         # print("weight, url", weight, url)
                         self.add_url_to_prioqueue(weight, link)
-            
-            
 
-
-    def compute_url_weight(self, player_count, date=None):
+    def compute_url_weight(self, player_count, date):
         # TODO add logic for computing the url weight
-        return 6
+        if int(date) < 2020 or player_count == 0:
+            # if the URL is outdated, return 10000 so it is never 
+            # added to the queue as this will be filtered later
+            return 10000
+        url_weight = -player_count + (2023 - date)
+        return url_weight
 
     def get_url_from_prioqueue(self):
         weight, url = self.urls_queue.get()
