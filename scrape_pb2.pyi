16bd68e5b018674d0e332e169dcb944d91255d5b
Sanjana Singh <sanjanasingh@college.harvard.edu>
2023-05-03 22:11:44 -0400

    recompile scrape.proto

--- scrape_pb2.pyi
+++ scrape_pb2.pyi
@@ -5,13 +5,15 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
-class Pair(_message.Message):
-    __slots__ = ["key", "value"]
-    KEY_FIELD_NUMBER: _ClassVar[int]
-    VALUE_FIELD_NUMBER: _ClassVar[int]
-    key: str
-    value: str
-    def __init__(self, key: _Optional[str] = ..., value: _Optional[str] = ...) -> None: ...
+class Data(_message.Message):
+    __slots__ = ["hyperlinks", "players_freq", "weight"]
+    HYPERLINKS_FIELD_NUMBER: _ClassVar[int]
+    PLAYERS_FREQ_FIELD_NUMBER: _ClassVar[int]
+    WEIGHT_FIELD_NUMBER: _ClassVar[int]
+    hyperlinks: List
+    players_freq: Dictionary
+    weight: int
+    def __init__(self, weight: _Optional[int] = ..., players_freq: _Optional[_Union[Dictionary, _Mapping]] = ..., hyperlinks: _Optional[_Union[List, _Mapping]] = ...) -> None: ...
 
 class Dictionary(_message.Message):
     __slots__ = ["pairs"]
@@ -25,18 +27,16 @@ class List(_message.Message):
     link: _containers.RepeatedScalarFieldContainer[str]
     def __init__(self, link: _Optional[_Iterable[str]] = ...) -> None: ...
 
-class Data(_message.Message):
-    __slots__ = ["hyperlinks", "players_freq", "weight"]
-    HYPERLINKS_FIELD_NUMBER: _ClassVar[int]
-    PLAYERS_FREQ_FIELD_NUMBER: _ClassVar[int]
-    WEIGHT_FIELD_NUMBER: _ClassVar[int]
-    hyperlinks: List
-    players_freq: Dictionary
-    weight: int
-    def __init__(self, weight: _Optional[int] = ..., players_freq: _Optional[_Union[Dictionary, _Mapping]] = ..., hyperlinks: _Optional[_Union[List, _Mapping]] = ...) -> None: ...
-
 class Message(_message.Message):
     __slots__ = ["message"]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
     message: str
     def __init__(self, message: _Optional[str] = ...) -> None: ...
+
+class Pair(_message.Message):
+    __slots__ = ["key", "value"]
+    KEY_FIELD_NUMBER: _ClassVar[int]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    key: str
+    value: str
+    def __init__(self, key: _Optional[str] = ..., value: _Optional[str] = ...) -> None: ...
